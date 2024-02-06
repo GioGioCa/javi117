0a8e0b4d05f1322b843649162aedabfaa681eea2
GioGioCa <darkconor2500@hotmail.com>
2024-02-05 23:34:16 -0600

    feat(About): Agregado contenido a la pagina About

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import  Layout from "../layout.jsx";
-
+export const mensaje = () => {
+  return (
+    <div  className="mensaje">
+      <h1>Hola soy Javier!!!</h1>
+      <p>
+        Jugador profesional en Apex con una gran habilidad en
+        caer e irme PAL lobby De día soy inge y de noche me gusta hacer stream
+        de juegos variados como mi serie de llegando a diamante en halo wars,
+        unas partidas del apextoso siempre ayudan, me gustan los juegos de
+        estrategia (RTS ), Gestión.
+      </p>
+    </div>
+  );
+};
 const About = () => {
   // Contenido de tu página About
   return (
     <main>
       <h1>About Page</h1>
+      {/* Llamada a la función que devuelve el contenido */}
+      {mensaje()}
       {/* Otros componentes o contenido */}
     </main>
   );
