ccc8ce86618db1255c5562c69c1d8804c6f6baee
GioGioCa <darkconor2500@hotmail.com>
2024-02-06 01:35:22 -0600

    styles(About): Agregados estilos a la pagina About

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,22 +1,35 @@
+import stylesAbout from '../styles/about.module.css';
+import Image from 'next/image';
+
 export const mensaje = () => {
   return (
-    <div  className="mensaje">
-      <h1>Hola soy Javier!!!</h1>
-      <p>
-        Jugador profesional en Apex con una gran habilidad en
-        caer e irme PAL lobby De día soy inge y de noche me gusta hacer stream
-        de juegos variados como mi serie de llegando a diamante en halo wars,
-        unas partidas del apextoso siempre ayudan, me gustan los juegos de
-        estrategia (RTS ), Gestión.
-      </p>
+    <div className={stylesAbout.container}>
+      <div className={stylesAbout.content}>
+        <h1>Hola soy Javier!!!</h1>
+        <p className={stylesAbout.parrafo}>
+          Jugador profesional en Apex con una gran habilidad en caer e irme PAL
+          lobby De día soy inge y de noche me gusta hacer stream de juegos
+          variados como mi serie de llegando a diamante en halo wars, unas
+          partidas del apextoso siempre ayudan, me gustan los juegos de
+          estrategia (RTS ), Gestión.
+        </p>
+      </div>
+      <div className={stylesAbout.image}>
+        <Image
+          src={"/img/aboutIMG.jpeg"}
+          width={100}
+          height={100}
+          alt="Inge"
+          priority={50}>
+        </Image>
+        </div>
     </div>
   );
 };
 const About = () => {
   // Contenido de tu página About
   return (
-    <main>
-      <h1>About Page</h1>
+    <main className={stylesAbout.aboutMain}>
       {/* Llamada a la función que devuelve el contenido */}
       {mensaje()}
       {/* Otros componentes o contenido */}
